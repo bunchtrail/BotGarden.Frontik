8553eb853f974ce9a8f850dd9c53b309bdaea5d4
Nikita Lolenko <loleenko@vk.com>
2024-11-23 12:43:54 +0300

    Add collapsible dropdown to Navbar for plant addition; enhance styling and responsiveness of Navbar and AddPlantPage

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 // src/components/Navbar/Navbar.tsx
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from '../../assets/styles/components/Navbar/Navbar.module.css';
 import LinkTitle from '../Misc/LinkTitle';
@@ -10,6 +10,7 @@ interface NavbarProps {
 }
 
 const Navbar: React.FC<NavbarProps> = ({ sectorId }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const formContext = useContext(FormContext);
 
   const saveData = () => {
@@ -33,9 +34,44 @@ const Navbar: React.FC<NavbarProps> = ({ sectorId }) => {
       <div className={styles.navbarContainer}>
         <div className={styles.navItems}>
           <Link to='/home' className={styles.navItem}>
-            <i className={`fas fa-home ${styles.icon}`} /> Вернуться на главный экран
+            <i className={`fas fa-home ${styles.icon}`} /> Вернуться на главный
+            экран
           </Link>
+
+          <div className={styles.dropdownContainer}>
+            <button
+              className={styles.dropdownButton}
+              onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+            >
+              <i className={`fas fa-plus ${styles.icon}`} />
+              Добавить растение в сектор {sectorId}
+              <i
+                className={`fas fa-chevron-down ${styles.dropdownIcon} ${
+                  isDropdownOpen ? styles.rotate : ''
+                }`}
+              />
+            </button>
+            <div
+              className={`${styles.dropdownContent} ${
+                isDropdownOpen ? styles.show : ''
+              } ${isDropdownOpen ? styles.animate : ''}`}
+            >
+              <Link to={`/add-plant/1`} className={styles.dropdownItem}>
+                <i className={`fas fa-tree ${styles.icon}`} />
+                Дендрология
+              </Link>
+              <Link to={`/add-plant/2`} className={styles.dropdownItem}>
+                <i className={`fas fa-leaf ${styles.icon}`} />
+                Флора
+              </Link>
+              <Link to={`/add-plant/3`} className={styles.dropdownItem}>
+                <i className={`fas fa-flower ${styles.icon}`} />
+                Цветоводство
+              </Link>
+            </div>
+          </div>
         </div>
+
         <div className={styles.buttonGroup}>
           <button
             className={styles.saveButton}
--- src/modules/Plant/pages/addPlantPage.tsx
+++ src/modules/Plant/pages/addPlantPage.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Input from '../../../components/Form/TextInput';
 import FormRow from '../../../components/Form/FormRow';
@@ -9,6 +9,13 @@ import Navbar from '../../../components/Navbar/Navbar';
 const AddPlantPage: React.FC = () => {
   const { sectorId } = useParams<{ sectorId: string }>();
   const formContext = useContext(FormContext);
+
+  const [isIdentificationOpen, setIdentificationOpen] = useState(true);
+  const [isClassificationOpen, setClassificationOpen] = useState(true);
+  const [isOriginOpen, setOriginOpen] = useState(true);
+  const [isUsageOpen, setUsageOpen] = useState(true);
+  const [isLocationOpen, setLocationOpen] = useState(true);
+  const [isAdditionalOpen, setAdditionalOpen] = useState(true);
 
   if (!formContext) {
     return <div>Loading...</div>;
@@ -28,187 +35,217 @@ const AddPlantPage: React.FC = () => {
           {sectorId ? parseInt(sectorId, 10) : 'неизвестно'}
         </h1>
         <form className={styles.form}>
-          <h2>Идентификация</h2>
-          <FormRow>
-            <Input
-              label='Inventor Number'
-              id='inventorNumber'
-              value={formData.inventorNumber}
-              onChange={handleChange}
-              placeholder='Введите номер инвентаризации'
-            />
-            <Input
-              label='Family ID'
-              id='familyId'
-              value={formData.familyId}
-              onChange={handleChange}
-              placeholder='Введите ID семейства'
-            />
-            <Input
-              label='Genus ID'
-              id='genusId'
-              value={formData.genusId}
-              onChange={handleChange}
-              placeholder='Введите ID рода'
-            />
-          </FormRow>
+          <h2 onClick={() => setIdentificationOpen(!isIdentificationOpen)} className={styles.collapsible}>
+            Идентификация
+            <span className={`${styles.caret} ${isIdentificationOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isIdentificationOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Inventor Number'
+                id='inventorNumber'
+                value={formData.inventorNumber}
+                onChange={handleChange}
+                placeholder='Введите номер инвентаризации'
+              />
+              <Input
+                label='Family ID'
+                id='familyId'
+                value={formData.familyId}
+                onChange={handleChange}
+                placeholder='Введите ID семейства'
+              />
+              <Input
+                label='Genus ID'
+                id='genusId'
+                value={formData.genusId}
+                onChange={handleChange}
+                placeholder='Введите ID рода'
+              />
+            </FormRow>
+          </div>
 
-          <h2>Классификация</h2>
-          <FormRow>
-            <Input
-              label='Species'
-              id='species'
-              value={formData.species}
-              onChange={handleChange}
-              placeholder='Введите вид'
-            />
-            <Input
-              label='Synonyms'
-              id='synonyms'
-              value={formData.synonyms}
-              onChange={handleChange}
-              placeholder='Введите синонимы'
-            />
-            <Input
-              label='Variety'
-              id='variety'
-              value={formData.variety}
-              onChange={handleChange}
-              placeholder='Введите разновидность'
-            />
-          </FormRow>
+          <h2 onClick={() => setClassificationOpen(!isClassificationOpen)} className={styles.collapsible}>
+            Классификация
+            <span className={`${styles.caret} ${isClassificationOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isClassificationOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Species'
+                id='species'
+                value={formData.species}
+                onChange={handleChange}
+                placeholder='Введите вид'
+              />
+              <Input
+                label='Synonyms'
+                id='synonyms'
+                value={formData.synonyms}
+                onChange={handleChange}
+                placeholder='Введите синонимы'
+              />
+              <Input
+                label='Variety'
+                id='variety'
+                value={formData.variety}
+                onChange={handleChange}
+                placeholder='Введите разновидность'
+              />
+            </FormRow>
+          </div>
 
-          <h2>Происхождение и Среда Обитания</h2>
-          <FormRow>
-            <Input
-              label='Plant Origin'
-              id='plantOrigin'
-              value={formData.plantOrigin}
-              onChange={handleChange}
-              placeholder='Введите происхождение растения'
-            />
-            <Input
-              label='Natural Habitat'
-              id='naturalHabitat'
-              value={formData.naturalHabitat}
-              onChange={handleChange}
-              placeholder='Введите естественную среду обитания'
-            />
-            <Input
-              label='Ecology Biology'
-              id='ecologyBiology'
-              value={formData.ecologyBiology}
-              onChange={handleChange}
-              placeholder='Введите экологию и биологию'
-            />
-          </FormRow>
+          <h2 onClick={() => setOriginOpen(!isOriginOpen)} className={styles.collapsible}>
+            Происхождение и Среда Обитания
+            <span className={`${styles.caret} ${isOriginOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isOriginOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Plant Origin'
+                id='plantOrigin'
+                value={formData.plantOrigin}
+                onChange={handleChange}
+                placeholder='Введите происхождение растения'
+              />
+              <Input
+                label='Natural Habitat'
+                id='naturalHabitat'
+                value={formData.naturalHabitat}
+                onChange={handleChange}
+                placeholder='Введите естественную среду обитания'
+              />
+              <Input
+                label='Ecology Biology'
+                id='ecologyBiology'
+                value={formData.ecologyBiology}
+                onChange={handleChange}
+                placeholder='Введите экологию и биологию'
+              />
+            </FormRow>
+          </div>
 
-          <h2>Использование и Защита</h2>
-          <FormRow>
-            <Input
-              label='Economic Use'
-              id='economicUse'
-              value={formData.economicUse}
-              onChange={handleChange}
-              placeholder='Введите экономическое использование'
-            />
-            <Input
-              label='Protection Status'
-              id='protectionStatus'
-              value={formData.protectionStatus}
-              onChange={handleChange}
-              placeholder='Введите статус защиты'
-            />
-          </FormRow>
+          <h2 onClick={() => setUsageOpen(!isUsageOpen)} className={styles.collapsible}>
+            Использование и Защита
+            <span className={`${styles.caret} ${isUsageOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isUsageOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Economic Use'
+                id='economicUse'
+                value={formData.economicUse}
+                onChange={handleChange}
+                placeholder='Введите экономическое использование'
+              />
+              <Input
+                label='Protection Status'
+                id='protectionStatus'
+                value={formData.protectionStatus}
+                onChange={handleChange}
+                placeholder='Введите статус защиты'
+              />
+            </FormRow>
+          </div>
 
-          <h2>Расположение</h2>
-          <FormRow>
-            <Input
-              label='Latitude'
-              id='latitude'
-              value={formData.latitude}
-              onChange={handleChange}
-              placeholder='Введите широту'
-            />
-            <Input
-              label='Longitude'
-              id='longitude'
-              value={formData.longitude}
-              onChange={handleChange}
-              placeholder='Введите долготу'
-            />
-            <Input
-              label='Country'
-              id='country'
-              value={formData.country}
-              onChange={handleChange}
-              placeholder='Введите страну'
-            />
-          </FormRow>
+          <h2 onClick={() => setLocationOpen(!isLocationOpen)} className={styles.collapsible}>
+            Расположение
+            <span className={`${styles.caret} ${isLocationOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isLocationOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Latitude'
+                id='latitude'
+                value={formData.latitude}
+                onChange={handleChange}
+                placeholder='Введите широту'
+              />
+              <Input
+                label='Longitude'
+                id='longitude'
+                value={formData.longitude}
+                onChange={handleChange}
+                placeholder='Введите долготу'
+              />
+              <Input
+                label='Country'
+                id='country'
+                value={formData.country}
+                onChange={handleChange}
+                placeholder='Введите страну'
+              />
+            </FormRow>
+          </div>
 
-          <h2>Дополнительная Информация</h2>
-          <FormRow>
-            <Input
-              label='Date Of Planting'
-              id='dateOfPlanting'
-              type='date'
-              value={formData.dateOfPlanting}
-              onChange={handleChange}
-            />
-            <Input
-              label='Date'
-              id='date'
-              type='date'
-              value={formData.date}
-              onChange={handleChange}
-            />
-            <Input
-              label='Originator'
-              id='originator'
-              value={formData.originator}
-              onChange={handleChange}
-              placeholder='Введите инициатора'
-            />
-          </FormRow>
-          <FormRow>
-            <Input
-              label='Herbarium Presence'
-              id='herbariumPresence'
-              value={formData.herbariumPresence}
-              onChange={handleChange}
-              placeholder='Введите наличие гербария'
-            />
-            <Input
-              label='Herbarium Duplicate'
-              id='herbariumDuplicate'
-              value={formData.herbariumDuplicate}
-              onChange={handleChange}
-              placeholder='Введите дубликат гербария'
-            />
-            <Input
-              label='Filled Out'
-              id='filledOut'
-              value={formData.filledOut}
-              onChange={handleChange}
-              placeholder='Введите заполнено'
-            />
-          </FormRow>
-          <FormRow>
-            <Input
-              label='Image Path'
-              id='imagePath'
-              value={formData.imagePath}
-              onChange={handleChange}
-              placeholder='Введите путь к изображению'
-            />
-            <Input
-              label='Note'
-              id='note'
-              value={formData.note}
-              onChange={handleChange}
-              placeholder='Введите заметку'
-            />
-          </FormRow>
+          <h2 onClick={() => setAdditionalOpen(!isAdditionalOpen)} className={styles.collapsible}>
+            Дополнительная Информация
+            <span className={`${styles.caret} ${isAdditionalOpen ? styles.rotate : ''}`}></span>
+          </h2>
+          <div className={`${styles.content} ${isAdditionalOpen ? styles.open : styles.closed}`}>
+            <FormRow>
+              <Input
+                label='Date Of Planting'
+                id='dateOfPlanting'
+                type='date'
+                value={formData.dateOfPlanting}
+                onChange={handleChange}
+              />
+              <Input
+                label='Date'
+                id='date'
+                type='date'
+                value={formData.date}
+                onChange={handleChange}
+              />
+              <Input
+                label='Originator'
+                id='originator'
+                value={formData.originator}
+                onChange={handleChange}
+                placeholder='Введите инициатора'
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                label='Herbarium Presence'
+                id='herbariumPresence'
+                value={formData.herbariumPresence}
+                onChange={handleChange}
+                placeholder='Введите наличие гербария'
+              />
+              <Input
+                label='Herbarium Duplicate'
+                id='herbariumDuplicate'
+                value={formData.herbariumDuplicate}
+                onChange={handleChange}
+                placeholder='Введите дубликат гербария'
+              />
+              <Input
+                label='Filled Out'
+                id='filledOut'
+                value={formData.filledOut}
+                onChange={handleChange}
+                placeholder='Введите заполнено'
+              />
+            </FormRow>
+            <FormRow>
+              <Input
+                label='Image Path'
+                id='imagePath'
+                value={formData.imagePath}
+                onChange={handleChange}
+                placeholder='Введите путь к изображению'
+              />
+              <Input
+                label='Note'
+                id='note'
+                value={formData.note}
+                onChange={handleChange}
+                placeholder='Введите заметку'
+              />
+            </FormRow>
+          </div>
         </form>
       </div>
     </FormProvider>
