b4768ebb6f0b0ddaa121b5bc373f9d5835e3b60b
Nikita Lolenko <loleenko@vk.com>
2024-12-18 14:51:09 +0300

    Remove unused map action handlers, components, and related styles to streamline the Map module

--- src/modules/Map/mapActions/removePlantHandler.ts
+++
@@ -1,8 +0,0 @@
-// src/config/mapActions/removePlantHandler.ts
-
-const removePlantHandler = () => {
-    console.log('Plant removed');
-  };
-  
-  export default removePlantHandler;
-  
--- src/modules/Map/pages/MapPage.tsx
+++ src/modules/Map/pages/MapPage.tsx
@@ -1,27 +1,11 @@
 // src/modules/Map/pages/MapPage.tsx
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import Navbar from '../../../components/Navbar/Navbar';
-import MapView, { MapViewRef } from '../components/MapView';
-import { useCustomMap } from '../hooks/useCustomMap';
-import { mapActionsConfig } from '../mapActions/mapActionsConfig';
-import { fetchMarkers, MarkerData } from '../services/mapService';
 import styles from './MapPage.module.css';
 
 const MapPage: React.FC = () => {
-  const [markers, setMarkers] = useState<MarkerData[]>([]);
-
-  useEffect(() => {
-    fetchMarkers().then(setMarkers);
-  }, []);
-  const { url: customMapUrl, bounds } = useCustomMap();
-
-  const mapViewRef = useRef<MapViewRef>(null);
-
   const handleAction = (action: string, file?: File) => {
-    const handler = mapActionsConfig[action];
-    if (handler) {
-      handler(mapViewRef, file);
-    } else {
+    {
       console.warn(`Нет обработчика для действия: ${action}`);
     }
   };
@@ -29,18 +13,7 @@ const MapPage: React.FC = () => {
   return (
     <div className={styles.mapPageContainer}>
       <Navbar pageType='map' onAction={handleAction} />
-      <div className={styles.mapWrapper}>
-        {bounds ? (
-          <MapView
-            ref={mapViewRef}
-            markers={markers}
-            customMapUrl={customMapUrl}
-            bounds={bounds}
-          />
-        ) : (
-          <div>Загрузка карты...</div>
-        )}
-      </div>
+      <div className={styles.mapWrapper}></div>
     </div>
   );
 };
